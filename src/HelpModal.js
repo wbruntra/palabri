62f6fa8ba5b97772592441295915d782178ea9af
William Bruntrager <w.bruntrager@gmail.com>
2022-01-21 10:29:49 -0500

    Clarify help modal

--- src/HelpModal.js
+++ src/HelpModal.js
@@ -14,10 +14,17 @@ export default function HelpModal({ show, handleClose }) {
           <p>Adivina la palabra del día según las pistas</p>
           <p>Ejemplos:</p>
           <Guess guess={{ word: 'TRUENO', key: '-GG---' }} />
-          <p className="my-3">La "U" y la "R" son correctas y bien colocadas</p>
+          <p className="my-3">La U y la R son correctas y bien colocadas</p>
           <Guess guess={{ word: 'PUNTAS', key: '-Y--G-' }} />
           <p className="my-3">
-            La A es correcta y en la posición correcta. La U es correcta pero debe estar en otra posición.
+            La A es correcta y en la posición correcta. La U es correcta pero debe estar en otra
+            posición.
+          </p>
+          <p>Los acentos no aparecen hasta que hayas adivinado la palabra entera.</p>
+          <p>
+            Palabras válidas: la palabra puede ser sustantivo en singular o plural. Si es verbo se
+            usa el infinitivo o el participio, no habrá conjugaciones raras (a no ser que haya, por
+            ejemplo, un sustantivo que se escribe igual).
           </p>
         </Modal.Body>
       </Modal>
